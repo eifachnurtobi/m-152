85c7a4c04edd99a79a61efb415db5ea37a28f2f8
Tobias Bazzan <tobias.bazzan@liip.ch>
2022-06-24 09:31:22 +0200

    Animation hinzufügen.

---
+++ src/components/Animation.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="w-full h-full p-8">
+        <div class="relative w-full h-full ">
+            <IconAnimation 
+                class="absolute h-10 w-10 -ml-5 -mt-5 transition-all ease-in-out duration-700"
+                :style="positions[position]"
+            />
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref } from 'vue';
+import IconAnimation from './icons/IconAnimation.vue';
+
+const positions = ref([
+    {top: 0, left: 0},
+    {top: 0, left: '100%'},
+    {top: '100%', left: '100%'},
+    {top: '100%', left: 0},
+]);
+
+const position = ref(0);
+const timer = ref();
+
+onMounted(() => {
+    timer.value = setInterval(() => {
+        position.value = (position.value + 1)%positions.value.length;
+    }, 1000);
+})
+
+onUnmounted(() => {
+    clearInterval(timer.value);
+})
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,18 +11,24 @@
             :width="700"
             :height="500"
             :images='[
-                {src: "/src/assets/logo.svg", alt: "Vue logo"},
-                {src: "/src/assets/logo.svg", alt: "Vue logo"},
-                {src: "/src/assets/logo.svg", alt: "Vue logo"},
-                {src: "/src/assets/logo.svg", alt: "Vue logo"},
-                {src: "/src/assets/logo.svg", alt: "Vue logo"},
-                {src: "/src/assets/logo.svg", alt: "Vue logo"},
+                {src: "https://picsum.photos/200/300", alt: "Vue logo"},
+                {src: "https://picsum.photos/800/100", alt: "Vue logo"},
+                {src: "https://picsum.photos/200/400", alt: "Vue logo"},
+                {src: "https://picsum.photos/300/300", alt: "Vue logo"},
+                {src: "https://picsum.photos/192/108", alt: "Vue logo"},
+                {src: "https://picsum.photos/1600/900", alt: "Vue logo"},
             ]'>
         </ImageGallery>
+        </div>
+
+        <h2 class="text-lg mt-8 mb-2">Animation</h2>
+        <div class="relative w-1/2 h-96 mx-auto">
+            <Animation/>
         </div>
     </div>
 </template>
 <script setup lang="ts">
 import ImageGallery from '@/components/ImageGallery.vue';
 import Video from '../components/Video.vue';
+import Animation from '../components/Animation.vue';
 </script>
